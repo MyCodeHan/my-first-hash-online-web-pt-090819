7038a9805926af559201c5bbb955ee1bb74317b8
Hannah Harrington <hannahharrington@outlook.com>
2019-09-26 21:41:11 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -2,8 +2,6 @@ def my_hash
     my_hash = {"name" => "Hannah", "age" => 33, "location" => "TX"}
  end
 
-
-
 def shipping_manifest
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 end
@@ -21,5 +19,4 @@ def adding
 
   shipping_manifest["pearl necklace"] = 1
   shipping_manifest
-end
 end
